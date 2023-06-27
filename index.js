1b17d1fc211833fa8f78ddcae998186d29e5cf57
Poonam <p_rajput@hotmail.co.uk>
2023-06-27 13:29:18 +0100

    initial commit and notes

---
+++ index.js
@@ -0,0 +1,15 @@
+/* TO DO
+- make a request to PokeAPI, see what the data looks like ( limit the generation)
+- design the table I want to create
+- create db and table
+- transform data (or leave in generations to remove the duplicates here)
+- insert it into the table
+
+ETL: Extract Transform Load
+    Takes data from multiple sources into a unified data repository
+    Can be used to take data from old systems, clean it up to establish consistency and then load data into a target database
+
+
+    
+
+*/
